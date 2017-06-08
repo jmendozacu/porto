8c27f4418fe3d5f71dc8ab39ff075f74f893b0d1
training2 <training2>
2017-06-08 02:54:23 -0700

    feat: init porto 2.5.

--- app/code/local/Smartwave/Porto/Block/Adminhtml/Button/Import/Cms.php
+++ app/code/local/Smartwave/Porto/Block/Adminhtml/Button/Import/Cms.php
@@ -5,6 +5,7 @@ class Smartwave_Porto_Block_Adminhtml_Button_Import_Cms extends Mage_Adminhtml_B
     protected function _getElementHtml(Varien_Data_Form_Element_Abstract $el)
     {
 		$data = $el->getOriginalData();
+        $helper = Mage::helper('porto');
 		if (isset($data['process']))
 			$process = $data['process'];
 		else
@@ -14,14 +15,22 @@ class Smartwave_Porto_Block_Adminhtml_Button_Import_Cms extends Mage_Adminhtml_B
 		if (isset($data['label']))
 			$buttonSuffix = ' ' . $data['label'];
 
-		$url = $this->getUrl('adminhtml/porto_import/' . $process);
-		
-		$html = $this->getLayout()->createBlock('adminhtml/widget_button')
-			->setType('button')
-			->setClass('import-cms')
-			->setLabel('Import' . $buttonSuffix)
-			->setOnClick("setLocation('$url')")
-			->toHtml();
+		$url = $this->getUrl('porto/adminhtml_import/' . $process);
+		$click_event = "setLocation('$url'+'demo_version/'+document.getElementById('porto_settings_install_demo_version').value)+'/'";
+        $button_class = "import-cms";
+        $after_html = "";
+        if(!$helper->checkPurchaseCode()) {
+            $click_event = "";
+            $button_class = "disabled import-cms";
+            $after_html = '<br/><em style="color:#f00;font-size:10px;line-height:1;">Activation is required.</em>';
+        }
+        $html = $this->getLayout()->createBlock('adminhtml/widget_button')
+            ->setType('button')
+            ->setClass($button_class)
+            ->setLabel('Import' . $buttonSuffix)
+            ->setOnClick($click_event)
+            ->toHtml();
+        $html .= $after_html;		
 			
         return $html;
     }
